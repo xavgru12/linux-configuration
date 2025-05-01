6353502b08417bef215526301dedfc58dfeacb50
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-05-01 20:10:04 +0200

    font

--- installNerdFonts.sh
+++ installNerdFonts.sh
@@ -1,4 +1,5 @@
 curl -fsSL https://raw.githubusercontent.com/ronniedroid/getnf/master/install.sh | bash
 getnf -i Hack
 echo try to open nvim and check if icons exist. If icons are not available, run: getnf and install all available nerd fonts
+#in gnome the custom font needs to be set: Preferences/profile/text/custom font -> select nerd font
 
